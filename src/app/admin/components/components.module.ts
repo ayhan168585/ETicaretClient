2cc0da9aea60f5b0394ed248643e9587a745520e
ayhan168585 <ayhan168585@hotmail.com>
2025-02-06 00:20:32 +0300

    Multiple layout yapılandırması tamamlandı

--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -4,6 +4,7 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { CustomersModule } from './customers/customers.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -14,7 +15,8 @@ import { DashboardModule } from './dashboard/dashboard.module';
     ProductsModule,
     OrdersModule,
     CustomersModule,
-    DashboardModule
+    DashboardModule,
+    RouterModule
   ]
 })
 export class ComponentsModule { }
